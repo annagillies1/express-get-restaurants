e9147e26917590bfcf522a1010f6af0756823544
annagillies1 <annagillies@hotmail.co.uk>
2024-10-15 10:13:16 +0100

    initial commit

--- src/app.js
+++ src/app.js
@@ -4,8 +4,11 @@ const Restaurant = require("../models/index")
 const db = require("../db/connection");
 
 //TODO: Create your GET Request Route Below: 
-
-
+// needs to be async in this line
+app.get("/restaurants", async (req, res) =>{
+    const restaurants = await Restaurant.findAll({});
+    res.json(restaurants);
+})
 
 
 module.exports = app;
